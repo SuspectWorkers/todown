73a9a8980d24c621d05678d2e171ef2ec3fd6ac8
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-12-06 08:36:27 +0200

    Create start2.sh

---
+++ start2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install wget curl -y
+if [ -f "qbee" ]
+then
+    echo "qbee binary exists."
+else
+    wget -O qbee https://github.com/userdocs/qbittorrent-nox-static/releases/download/release-4.4.4_v2.0.7/x86_64-qbittorrent-nox
+fi
+mkdir -p qBittorrent/config/
+if [ -f "qBittorrent/config/qBittorrent.conf" ]
+then
+    echo "qbee conf file exists."
+else
+    echo '[AutoRun]
+    enabled=true
+    program="/content/rclone --config=\"/content/rclone1/rclone.conf\" move \"%F\" \"drive:/mine/%N\""
+    [BitTorrent]
+    Session\DefaultSavePath=/content/qBittorrent/downloads
+    Session\ExcludedFileNames=
+    Session\GlobalMaxRatio=0
+    Session\QueueingSystemEnabled=true
+    Session\TempPath=/content/qBittorrent/downloads/temp
+    [LegalNotice]
+    Accepted=true
+    [Preferences]
+    WebUI\AuthSubnetWhitelistEnabled=false
+    WebUI\Address=*
+    WebUI\ServerDomains=*
+    WebUI\Port=10156
+    WebUI\HostHeaderValidation=false
+    WebUI\ClickjackingProtection=false
+    WebUI\CSRFProtection=false
+    WebUI\LocalHostAuth=true' > /content/qBittorrent/config/qBittorrent.conf
+fi
